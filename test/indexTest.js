8158f73748706b67f8b709aa7b5f2819946125b5
github username <github email address>
2017-09-26 15:50:05 -0400

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -40,7 +40,7 @@ describe('index.js', function () {
     });
 
     it("invokes 'Math.floor()'", function () {
-      expect(spyOnMathFloor.calledOnce).to.be.true;
+     expect(spyOnMathFloor.calledOnce).to.be.true;
     });
   });
 
