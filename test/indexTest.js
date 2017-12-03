a89d9b800d01db915d2eba2ebe432fe1fda33115
Monika Williams <monika.m.williams@gmail.com>
2017-12-03 03:06:08 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -28,7 +28,7 @@ describe('index.js', function () {
     });
 
     it("invokes 'Math.random('')", function () {
-      expect(spyOnMathRandom.calledOnce).to.be.false;
+      expect(spyOnMathRandom.calledOnce).to.be.true;
     });
   });
 
