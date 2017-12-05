2712bf7f1a4474437eeeb419f9890838a2bdbeec
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-05 17:24:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-/*
-Variables defined in the test suite:
-- oldID
-- currentAge
 
-You should NOT try to redefine any of the above variables.
-*/
 
 // Write your code below this line!
+let newID = oldID + 1000000000;
+let ageIsValid = Number.isInteger(currentAge)
+let randomNumber = Math.random() * 20;
+let randomInteger =  Math.floor(randomNumber);
+let randomUserID = Math.floor(randomNumber) + 1000000000;
