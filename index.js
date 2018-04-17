7a35982a4dd475a6648f565a847827ed23b1fdff
catalanowebdevelopment <catalano.work@gmail.com>
2018-04-17 10:09:15 -0400

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const randomNumber = Math.random() * 20
 
 const randomInteger = Math.floor(randomNumber)
 
-const randomUserID = Math.floor(Math.random() * 20) + 1000000000
+const randomUserID = randomInteger + 1000000000
 
 
 
