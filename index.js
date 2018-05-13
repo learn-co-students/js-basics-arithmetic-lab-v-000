e0b974f6b653d5f94b631c236c675f34d45ba6d5
github username apalski <github email address annette.drapalski@gmail.com>
2018-05-13 20:29:15 +1000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-/*
-Variables defined in the test suite:
-- oldID
-- currentAge
+const newID = 1000000000 + oldID;
+//
+let ageIsValid;
 
-You should NOT try to redefine any of the above variables.
-*/
+if (Number.isInteger(currentAge)) {
+  ageIsValid = true;
+} else {
+  ageIsValid = false;
+}
 
-// Write your code below this line!
+let randomNumber = (Math.floor(Math.random() * 20)) + 1;
+
+let randomInteger = Math.round(randomNumber);
+
+let randomUserID = 1000000000 + randomInteger;
