1db6ca3af44077c79bd65dc329ef065ef796ae8d
Ethel C <cofresi.ethel@gmail.com>
2018-10-25 05:12:30 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,30 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+//newID
+//converts IDs from the old style to the new style ‣
+
+let newID = oldID + 1000000000;
+
+//ageIsValid
+//contains 'true' when the value in 'currentAge' is a valid integer ‣
+//invokes 'Number.isInteger()' ‣
+
+let ageIsValid =Number.isInteger(currentAge);
+
+//randomNumber
+//contains a random number greater than or equal to 0 and less than 20 ‣
+//invokes 'Math.random()' ‣
+
+let randomNumber = (Math.random() * 20);
+
+//randomInteger
+//contains the value of 'randomNumber' rounded down to the nearest integer ‣
+
+let randomInteger = Math.floor(randomNumber);
+
+//randomUserID
+//contains a random integer between 1000000001 and 1000000020
+
+let randomUserID = randomInteger + 1000000001;
