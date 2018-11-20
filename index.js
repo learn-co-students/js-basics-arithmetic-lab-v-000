4ecb7c0c7018481c2d8206fcdd6f1af49f82a97f
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-20 00:58:21 +0000

    Done.

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ let newID = oldID + 1000000000;
 
 let ageIsValid = Number.isInteger(currentAge);
 
-let randomNumber = Math.random(Number * 20);
+let randomNumber = Math.random(Number * 20) + 1;
 
 let randomInteger = Math.floor(randomNumber);
 
-let randomUserID = randomInteger + newID;
+let randomUserID = randomInteger + 1000000000;
 
