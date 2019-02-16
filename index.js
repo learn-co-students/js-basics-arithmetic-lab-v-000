443c0d63fab5728669d408b339842cd28e06058a
github username <github email address>
2019-02-16 12:13:45 -0600

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,19 @@ Variables defined in the test suite:
 
 You should NOT try to redefine any of the above variables.
 */
-
+const newID = oldID + 1000000000;
 // Write your code below this line!
+const ageIsValid = Number.isInteger(currentAge);
+//const oldID = Math.floor(Math.random() * 100) + 1;
+const randomNumber = Math.random();
+//const ages = [26.4, 21, 49.5, 83, 99, 53.3, 74, 94.9, 40, 56.2];
+const randomInteger = Math.floor(randomNumber);
+//const currentAge = ages[Math.floor(Math.random() * ages.length)];
+const randomUserID = Math.floor(Math.random() * (1000000020 - 1000000001) + 1000000001);
+//const currentAgeIsInteger = Number.isInteger(currentAge);
+
+//const spyOnNumberIsInteger = sinon.stub(Number, 'isInteger').returns(currentAgeIsInteger);
+
+//const randNum = Math.random();
+
+//const spyOnMathRandom = sinon.stub(Math, 'random').returns(randNum);
