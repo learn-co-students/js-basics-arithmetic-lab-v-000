7b766ddfa2fbfd7ebb5da6594b99cc7803d92c83
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-08-08 21:04:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here
+
+let var1 = 2,
+    var2 = 31;
+    
+const multiply = var1 * var2;
+
+const random = Math.random();
+
+const mod = (var2+1) % 7;
+
+const max = Math.max(20)
