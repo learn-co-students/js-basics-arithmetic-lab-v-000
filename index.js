4682c9bd8db6eca965918274fa17e785338f8bc4
BusterStatus <matthew.serre@gmail.com>
2019-08-12 16:22:09 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here
+let var1 = 31;
+let var2 = 2;
+let multiply;
+multiply = var1 * var2;
+
+let random;
+random = Math.random() * 10;
+
+let mod;
+mod = 16 % 6;
+
+let max;
+max = Math.max(1, 19, 20);
