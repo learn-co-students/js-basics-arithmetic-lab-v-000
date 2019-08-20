4544ed96a9188551a3d3c7d98452e146ebde5d63
Erica Murdock-Waters <ericahpa@gmail.com>
2019-08-20 04:13:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here
+//multiply is an equation whose multiplied results = 62
+let var1 = 12.4
+let var2 = 5
+const multiply = var1 * var2
+
+//random generates a random integer greater than 0
+let random = Math.random()
+
+//mod generates a remainder that is equal to 4
+const peach = 5
+let mod = 9 % peach
+
+// max will return 20 as the highest number in the set
+const max = Math.max(3, 17, 9, 12, 20, 0)
