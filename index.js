51fc27aeca274a79b944da1a007717bed6046874
leothebeagle <a.kosber@gmail.com>
2019-08-24 18:27:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here
+let var1 = 2;
+let var2 = 31;
+const multiply = var1 * var2;
+
+const random = Math.random();
+
+const mod = 16 % 12;
+
+const max = Math.max(20, 19, 18, 17);
