1e81dbb8ea6542f6312d15600cc67ad810c1932b
Rebecca Tong <rtong@buffalo.edu>
2019-08-26 15:41:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here
+
+let random = Math.random() + 0;
+
+let max = Math.max(1, 2, 3, 4, 5, 20);
+
+let mod = 19 % 5;
+
+let var1 = 62;
+let var2 = 1;
+
+let multiply = var1 * var2;
