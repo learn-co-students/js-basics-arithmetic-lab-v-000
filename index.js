fad6b30b0baac1fabe6ee1dba38f83b8643f7fe9
Monica Marziano <mmarziano1@gmail.com>
2019-08-30 15:47:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here
+
+let var1 = 31;
+let var2 = 2;
+
+let multiply = var1 * var2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = Math.floor(24/5);
+
+let max = Math.max(1, 20, 13, 4, 15);
