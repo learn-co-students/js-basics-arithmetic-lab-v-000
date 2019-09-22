daa7bf076711cdd5bcfcb494ab3028fc1b030de1
danetter17 <etter.dt@gmail.com>
2019-09-22 18:40:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const var1 = 31;
 const var2 = 2;
 const multiply = var1 * var2;
-const random = Math.floor(Math.random() * 100) + 1;
+const random = Math.floor(Math.random() * 10) + 1;
 const mod = 24 % 5;
 const max = Math.max(1,2,20);
