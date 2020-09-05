79f4751aa2a2c110a6abeafc645bf4e921c340d2
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-09-05 05:19:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here
+let var1 = 2 
+let var2 = 31;
+const multiply = var1*var2;
+
+let random = Math.floor(Math.random);
+
+let mod = 4%30;
+
+let max = Math.max(2, 3, 5, 10, 20);
