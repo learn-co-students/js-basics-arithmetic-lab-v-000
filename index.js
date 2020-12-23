a025633b41d2c56722896940cd7e7a1222627bb3
John Dust <d24dust@hotmail.com>
2020-12-23 20:45:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here
+let var1 = 31
+let var2 = 2
+
+const multiply = var1 * var2
+
+let random = Math.ceil(Math.random * 69)
+
+const mod = 4 % 6
+
+const max = Math.max(1, 6, 9, 20)
