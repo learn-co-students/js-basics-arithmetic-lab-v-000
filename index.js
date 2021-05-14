25b00ba2df4b81bcdcd17dfc1fe816b04920e914
CharlesOK <okwechimeamechi@gmail.com>
2021-05-13 20:44:59 -0400

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ let var2 = 31;
 let multiply = var1 * var2
 let random = Math.floor(Math.random() * 10) + 1
 let mod = 24 % 5;
-let max  = Math.max(1, 2, 3, 4, 6, 20)
+let max  = Math.max(5, 10, 15, 20)
